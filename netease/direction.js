6b01ba92d6cd50c447c1713aa14af5bfbe977ad6
ILNILEY <ilniley@163.com>
2018-08-03 10:13:00 +0800

    addd：方向转向

---
+++ netease/direction.js
@@ -0,0 +1,45 @@
+/**
+ * 牛牛去犇犇老师家补课，出门的时候面向北方，但是现在他迷路了。虽然他手里有一张地图，但是他需要知道自己面向哪个方向，请你帮帮他。
+ * 每个输入包含一个测试用例。
+ * 每个测试用例的第一行包含一个正整数，表示转方向的次数N(N<=1000)。
+ * 接下来的一行包含一个长度为N的字符串，由L和R组成，L表示向左转，R表示向右转。
+ * 输出：输出牛牛最后面向的方向，N表示北，S表示南，E表示东，W表示西
+ */
+
+let N = 0;
+let str = '';
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.on('line', (value) => {
+    if (N === 0) {
+        N = value;
+    }else if (str === '') {
+        str = value;
+    }
+    if (N !== 0 && str !== '') {
+        rl.close();
+    }
+}).on('close', () => {
+    let directionArr = ['N', 'E', 'S', 'W'];
+    let currentPos = 0;
+    for (let item of str) {
+        if (item === 'L') {
+            if (currentPos === 0) {
+                currentPos = directionArr.length - 1;
+            } else {
+                currentPos -= 1;
+            }
+        } else {
+            if (currentPos === directionArr.length - 1) {
+                currentPos = 0;
+            } else {
+                currentPos += 1;
+            }
+        }
+    }
+    console.log(directionArr[currentPos]);
+    process.exit(0);
+})
